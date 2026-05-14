602edc547436133554111a135f88bbf1532ead34
Вера <verakom72@gmail.com>
2026-05-14 12:20:14 +0300

    исправлен host на 127.0.0.1

--- Crime_stat/vera_crime_server.py
+++ Crime_stat/vera_crime_server.py
@@ -120,4 +120,4 @@ def process():
 
 if __name__ == '__main__':
     print("Server running at http://127.0.0.1:5001")
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='127.0.0.1', port=5001, debug=False)
